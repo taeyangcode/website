373043e5b52c6529d60b19500c3878febe4d5966
Corey Mostero <mostero@berkeley.edu>
2026-05-20 16:16:00 -0700

    wip: first version

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,62 +1,65 @@
-import { Button } from "#/components/ui/button";
-import { cn } from "#/lib/utils";
+import { Clipboard } from "@effect/platform-browser";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { Effect, Schema } from "effect";
+
 import OskiImage from "#/assets/images/oski.png";
+import SamoyedImage from "#/assets/images/samoyed.png";
 
 export const Route = createFileRoute("/")({ component: Home });
 
-type Language = "english" | "korean";
-
 function Home() {
-  const [activeLanguage, setActiveLanguage] = useState<Language>("english");
-
   return (
-    <main className="flex max-w-lg flex-col gap-4 p-8">
-      <LanguageSelector activeLanguage={activeLanguage} onLanguageChange={setActiveLanguage} />
-
+    <main className="flex flex-col gap-6 p-8 sm:max-w-xl">
       <div className="flex flex-col gap-2">
         <img
           src={OskiImage}
           alt="Oski Bear"
+          draggable={false}
           style={{ imageRendering: "pixelated" }}
           className="h-20 w-20"
         />
         <p className="font-pixel">
-          Hello. My name is Corey Mostero and I like to build fault-tolerant software. I graduated
-          from Berkeley studying Applied Mathematics.
+          Hello. My name is Corey Mostero. I graduated in 2026 from Berkeley studying Applied
+          Mathematics.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <img
+          src={SamoyedImage}
+          alt="Samoyed"
+          draggable={false}
+          style={{ imageRendering: "pixelated" }}
+          className="h-20 w-20"
+        />
+        <p className="font-pixel text-balance">
+          Some things I like are compile time safety, Samoyeds, cafes, and truth. I love to make
+          friends, so please reach out{" "}
+          <button
+            type="button"
+            onClick={() => Effect.runPromise(copyEmail)}
+            className="peer underline underline-offset-2"
+          >
+            here
+          </button>
+          ! <span className="invisible peer-active:visible"> (copied to clipboard)</span>
         </p>
       </div>
     </main>
   );
 }
 
-type LanguageSelectorProps = {
-  activeLanguage: Language;
-  onLanguageChange: React.Dispatch<React.SetStateAction<Language>>;
-};
+const copyEmail = Effect.gen(function* () {
+  const clipboard = yield* Clipboard.Clipboard;
 
-function LanguageSelector(props: LanguageSelectorProps) {
-  return (
-    <div className="flex flex-row">
-      <Button
-        variant="link"
-        onClick={() => props.onLanguageChange("english")}
-        className={cn("transition-none", {
-          underline: props.activeLanguage === "english",
-        })}
-      >
-        ENG
-      </Button>
-      <Button
-        variant="link"
-        onClick={() => props.onLanguageChange("korean")}
-        className={cn("transition-none", {
-          underline: props.activeLanguage === "korean",
-        })}
-      >
-        KOR
-      </Button>
-    </div>
+  const contentResponse = yield* Effect.tryPromise(() => fetch("/config.json"));
+  const contentJson = yield* Effect.tryPromise(() =>
+    Schema.decodeUnknownPromise(Schema.Json)(contentResponse.json()),
   );
-}
+  const content = yield* Effect.tryPromise(() =>
+    Schema.decodeUnknownPromise(Schema.Record(Schema.String, Schema.String))(contentJson),
+  );
+  const [a, b, c, d] = Object.entries(content).toSorted(([a, _], [b, __]) => a.localeCompare(b));
+  const result = `${a[0]}${a[1]}${b[0]}${b[1]}@${c[0]}${c[1]}${d[0]}.${d[1]}`;
+  yield* clipboard.writeString(result);
+}).pipe(Effect.provide(Clipboard.layer));
