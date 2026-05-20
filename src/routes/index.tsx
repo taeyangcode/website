b7a9f2b4f773cb47af6ac1b93b3c812fc8d8130f
Corey Mostero <mostero@berkeley.edu>
2026-05-20 13:25:22 -0700

    wip: start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,62 @@
+import { Button } from "#/components/ui/button";
+import { cn } from "#/lib/utils";
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import OskiImage from "#/assets/images/oski.png";
 
 export const Route = createFileRoute("/")({ component: Home });
 
+type Language = "english" | "korean";
+
 function Home() {
+  const [activeLanguage, setActiveLanguage] = useState<Language>("english");
+
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+    <main className="flex max-w-lg flex-col gap-4 p-8">
+      <LanguageSelector activeLanguage={activeLanguage} onLanguageChange={setActiveLanguage} />
+
+      <div className="flex flex-col gap-2">
+        <img
+          src={OskiImage}
+          alt="Oski Bear"
+          style={{ imageRendering: "pixelated" }}
+          className="h-20 w-20"
+        />
+        <p className="font-pixel">
+          Hello. My name is Corey Mostero and I like to build fault-tolerant software. I graduated
+          from Berkeley studying Applied Mathematics.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+type LanguageSelectorProps = {
+  activeLanguage: Language;
+  onLanguageChange: React.Dispatch<React.SetStateAction<Language>>;
+};
+
+function LanguageSelector(props: LanguageSelectorProps) {
+  return (
+    <div className="flex flex-row">
+      <Button
+        variant="link"
+        onClick={() => props.onLanguageChange("english")}
+        className={cn("transition-none", {
+          underline: props.activeLanguage === "english",
+        })}
+      >
+        ENG
+      </Button>
+      <Button
+        variant="link"
+        onClick={() => props.onLanguageChange("korean")}
+        className={cn("transition-none", {
+          underline: props.activeLanguage === "korean",
+        })}
+      >
+        KOR
+      </Button>
     </div>
   );
 }
